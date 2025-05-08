96e30fc0237bec1db6ec301f5bb6889649040001
Malek <mohamedmalekalfakih@istic.ucar.tn>
2025-05-08 23:41:31 +0200

    qq

--- docs/conf.py
+++ docs/conf.py
@@ -8,13 +8,37 @@ author = 'Mohamed Malek Al Fakih & Abdellah Bichlifen'
 release = '0.1.0'
 
 extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.napoleon',
-'sphinx.ext.viewcode'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
+# -- HTML output -------------------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'display_version': True,
+    'style_external_links': True,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2980B9',  # Optional: customize
+    'display_github': True,
+    'github_user': 'MalekAlFakih',
+    'github_repo': 'quantum-anomaly-detection',
+    'github_version': 'main',
+    'conf_py_path': '/docs/',
+}
+
+html_context = {
+    "display_github": True,
+    "github_user": "MalekAlFakih",
+    "github_repo": "quantum-anomaly-detection",
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+}
+
 html_static_path = ['_static']
